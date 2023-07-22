51fcde219c5fb0c2d881596c6472b132474c55d3
keigo mochiduki <keeee50114@gmail.com>
2023-07-22 21:08:38 +0900

    inputに入力したものが、未完了タスクに入る

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグ生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //liタグの中に各要素を設定設定
+  li.appendChild(div);
+
+  //divタグの中に各要素を設定設定
+  div.appendChild(p);
+
+  //未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
